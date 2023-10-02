329eb2a8b42814f2aebbd37e3d9aa8e2372b27dd
Marcosaure7 <magou277@ulaval.ca>
2023-10-02 13:14:46 -0400

    Configuration console

--- src/application/App.java
+++ src/application/App.java
@@ -9,33 +9,35 @@ import java.util.ArrayList;
 public class App {
 
     private static final String LIGNE = "***********************************************************************************************************";
-    private ArrayList<Arme> banqueArmes = genererBanqueArmes();
-    private ArrayList<Ennemi> banqueEnnemis = genererEnnemis();
-    private Joueur joueur = new Joueur(0);
+    private static ArrayList<Arme> banqueArmes = genererBanqueArmes();
+    private static ArrayList<Ennemi> banqueEnnemis = genererEnnemis();
+    private static Joueur joueur = new Joueur(0);
 
     public static void main(String[] args) {
 
         System.out.println(LIGNE + "\nBienvenue!\n" + LIGNE);
 
-
+        Ennemi ennemiActuel = spawnerProchainEnnemi();
+        System.out.println("Attention, votre premier ennemi!");
 
     }
 
-    private ArrayList<Arme> genererBanqueArmes() {
+    private static ArrayList<Arme> genererBanqueArmes() {
         ArrayList<Arme> armeArrayList = new ArrayList<>();
         armeArrayList.add(new Arme("Gourdin", 1, null));
         armeArrayList.add(new Arme("Hache", 0, EffetStatut.SAIGNEMENT));
         return armeArrayList;
     }
 
-    private ArrayList<Ennemi> genererEnnemis() {
+    private static ArrayList<Ennemi> genererEnnemis() {
         ArrayList<Ennemi> ennemiArrayList = new ArrayList<>();
         ennemiArrayList.add(new Ennemi("Squelette", joueur.getNiveau(), 5));
         ennemiArrayList.add(new Ennemi("Ogre", joueur.getNiveau(), 2));
         return ennemiArrayList;
     }
 
-    private Ennemi spawnerProchainEnnemi() {
+    private static Ennemi spawnerProchainEnnemi() {
+
         return
     }
 }
