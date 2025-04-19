fa8f51e4e950ad22efd667d9fe83728db96bc909
Marcosaure7 <magou277@ulaval.ca>
2025-04-19 19:57:34 -0400

    Added ssh tunneling

--- Kingdom-Fall-UI-Enabled/src/main/java/application/DatabaseManager.java
+++ Kingdom-Fall-UI-Enabled/src/main/java/application/DatabaseManager.java
@@ -1,27 +1,60 @@
 package application;
 
 import java.sql.*;
+
+import com.jcraft.jsch.JSchException;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import io.github.cdimascio.dotenv.Dotenv;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 public class DatabaseManager {
     private MysqlDataSource dataSource;
     private Connection conn;
+    Session session = null;
 
     public DatabaseManager() {
 
-        Dotenv dotenv = Dotenv.configure().load();
-
         try {
+
+            Dotenv dotenv = Dotenv.configure().load();
+            boolean sessionSsh = dotenv.get("SSH_REQUIRED").equals("true");
+
+            String dbHost = dotenv.get("DB_HOST");
+            int dbPort = Integer.parseInt(dotenv.get("DB_PORT"));
+            String dbUser = dotenv.get("DB_USER");
+            String dbPassword = dotenv.get("DB_PASSWORD");
+
+            if (sessionSsh) {
+                String sshHost = dotenv.get("SSH_HOST");
+                String sshUser = dotenv.get("SSH_USER");
+                String sshPrivateKey = System.getProperty("user.home") + "/.ssh/id_rsa";
+                int sshPort = Integer.parseInt(dotenv.get("SSH_PORT"));
+
+                JSch jsch = new JSch();
+                jsch.addIdentity(sshPrivateKey); // Ajouter la clé privée SSH
+
+                // Créer une session SSH
+                session = jsch.getSession(sshUser, sshHost, sshPort);
+                session.setConfig("StrictHostKeyChecking", "no");
+                session.connect();
+
+                // Configurer le tunnel SSH (port forwarding local)
+                int assignedPort = session.setPortForwardingL(dbPort, dbHost, dbPort);
+                System.out.println("Tunnel SSH établi sur localhost:" + assignedPort);
+            }
+
             dataSource = new MysqlDataSource();
-            dataSource.setURL(String.format("jdbc:mysql://%s:%s/%s", dotenv.get("DB_HOST"), dotenv.get("DB_PORT"), dotenv.get("DB_NAME")));
-            dataSource.setUser(dotenv.get("DB_USER"));
-            dataSource.setPassword(dotenv.get("DB_PASSWORD"));
+            dataSource.setURL(String.format("jdbc:mysql://%s:%s/%s", dbHost, dbPort, dotenv.get("DB_NAME")));
+            dataSource.setUser(dbUser);
+            dataSource.setPassword(dbPassword);
             conn = dataSource.getConnection();
         }
 
         catch (SQLException sqlException) {
             sqlException.printStackTrace();
+        } catch (JSchException e) {
+            e.printStackTrace();
         }
 
     }
